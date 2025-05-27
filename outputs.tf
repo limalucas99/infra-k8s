83e092035ea9a47eacd10066a281829ff28e572f
Felipe Cruz <45316668+feligra@users.noreply.github.com>
2025-05-27 20:19:56 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_name" {
-  value = module.eks.cluster_name
+  value = aws_eks_cluster.eks.name
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+  value = aws_eks_cluster.eks.certificate_authority[0].data
 }
