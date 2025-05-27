3d12db3f0aa8e160a2e6ecda86ca8b5e73d81369
Felipe Cruz <45316668+feligra@users.noreply.github.com>
2025-05-27 19:59:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,39 +2,67 @@ provider "aws" {
   region = "us-east-1"
 }
 
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.2"
-
-  name = "api-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
+# Role manual do Academy (já existente, como a labRole)
+data "aws_iam_role" "eks_cluster_role" {
+  name = "labRole" # Troque se for outro nome da sua role
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.0.0"
+# VPC simplificada (ou use a que já existe)
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  cluster_name    = "api-cluster"
-  cluster_version = "1.29"
-  subnet_ids      = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
+resource "aws_subnet" "subnet_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
 
-  eks_managed_node_groups = {
-    app_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
+resource "aws_subnet" "subnet_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
 
-      instance_types = ["t2.micro"]
+# Security Group para o cluster
+resource "aws_security_group" "eks_cluster_sg" {
+  name        = "eks-cluster-sg"
+  description = "EKS cluster SG"
+  vpc_id      = aws_vpc.main.id
+}
 
-      iam_role_name = "LabRole"
-    }
+# EKS Cluster
+resource "aws_eks_cluster" "eks" {
+  name     = "academy-cluster"
+  role_arn = data.aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+    security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
+
+  version = "1.29"
+
+  depends_on = [aws_security_group.eks_cluster_sg]
+}
+
+# IAM Role para o Node Group (pode ser a mesma do labRole ou outra criada manualmente)
+data "aws_iam_role" "eks_node_role" {
+  name = "labRole" # Reutilize a mesma se necessário
+}
+
+# Node Group gerenciado
+resource "aws_eks_node_group" "node_group" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "academy-node-group"
+  node_role_arn   = data.aws_iam_role.eks_node_role.arn
+  subnet_ids      = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t2.micro"]
 }
