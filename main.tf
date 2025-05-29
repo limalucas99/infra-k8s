da77462c70b3dc41794182931ef6fc8b53a76706
Felipe Cruz <45316668+feligra@users.noreply.github.com>
2025-05-29 20:07:41 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,13 +15,13 @@ provider "aws" {
 }
 
 #########################################################
-# 1. IAM Roles (já existentes no AWS Academy)
+# 1. IAM Roles (pré-existentes no AWS Academy)
 #########################################################
 data "aws_iam_role" "eks_cluster_role" { name = "labRole" }
 data "aws_iam_role" "eks_node_role"    { name = "labRole" }
 
 #########################################################
-# 2. VPC pública minimalista (duas AZs)
+# 2. VPC pública minimalista (2 AZs)
 #########################################################
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -33,7 +33,11 @@ resource "aws_internet_gateway" "gw" {
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route { cidr_block = "0.0.0.0/0"  gateway_id = aws_internet_gateway.gw.id }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 }
 
 locals {
@@ -61,15 +65,21 @@ resource "aws_security_group" "eks_cluster_sg" {
   name        = "eks-cluster-sg"
   description = "EKS cluster SG"
   vpc_id      = aws_vpc.main.id
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 #########################################################
-# 3. EKS Cluster (ignora alteração interna do provider)
+# 3. Cluster EKS
 #########################################################
 resource "aws_eks_cluster" "eks" {
-  name    = "academy-cluster"
-  version = "1.29"
+  name     = "academy-cluster"
+  version  = "1.29"
   role_arn = data.aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
@@ -78,7 +88,7 @@ resource "aws_eks_cluster" "eks" {
   }
 
   lifecycle {
-    ignore_changes = [ bootstrap_self_managed_addons ]
+    ignore_changes = [bootstrap_self_managed_addons]
   }
 }
 
@@ -90,9 +100,15 @@ resource "aws_eks_node_group" "node_group" {
   node_group_name = "academy-node-group"
   node_role_arn   = data.aws_iam_role.eks_node_role.arn
   subnet_ids      = values(aws_subnet.public)[*].id
-  instance_types  = ["t3.micro"]
 
-  scaling_config { desired_size = 2 max_size = 3 min_size = 1 }
+  instance_types = ["t3.micro"]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
   timeouts { create = "30m" }
 }
 
