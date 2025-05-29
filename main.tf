ba5dca906401ce7a480c7fd517cbcd27b64cfb6d
Felipe Cruz <45316668+feligra@users.noreply.github.com>
2025-05-29 00:10:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,21 +15,34 @@ provider "aws" {
 }
 
 #########################################################
-# 1. IAM Roles
+# 1. IAM Roles (pré-existentes no AWS Academy)
 #########################################################
-data "aws_iam_role" "eks_cluster_role" { name = "labRole" }
-data "aws_iam_role" "eks_node_role"    { name = "labRole" }
+data "aws_iam_role" "eks_cluster_role" {
+  name = "labRole"
+}
+
+data "aws_iam_role" "eks_node_role" {
+  name = "labRole"
+}
 
 #########################################################
 # 2. VPC pública simples
 #########################################################
-resource "aws_vpc" "main" { cidr_block = "10.0.0.0/16" }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
 
-resource "aws_internet_gateway" "gw" { vpc_id = aws_vpc.main.id }
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  route  { cidr_block = "0.0.0.0/0"; gateway_id = aws_internet_gateway.gw.id }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
 }
 
 resource "aws_subnet" "subnet_a" {
@@ -38,6 +51,7 @@ resource "aws_subnet" "subnet_a" {
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
+
 resource "aws_subnet" "subnet_b" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
@@ -49,6 +63,7 @@ resource "aws_route_table_association" "a" {
   subnet_id      = aws_subnet.subnet_a.id
   route_table_id = aws_route_table.public.id
 }
+
 resource "aws_route_table_association" "b" {
   subnet_id      = aws_subnet.subnet_b.id
   route_table_id = aws_route_table.public.id
@@ -59,11 +74,16 @@ resource "aws_security_group" "eks_cluster_sg" {
   description = "EKS cluster SG"
   vpc_id      = aws_vpc.main.id
 
-  egress { from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = ["0.0.0.0/0"] }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 #########################################################
-# 3. Cluster EKS  (sem prevent_destroy)
+# 3. Cluster EKS
 #########################################################
 resource "aws_eks_cluster" "eks" {
   name     = "academy-cluster"
@@ -87,16 +107,28 @@ resource "aws_eks_node_group" "node_group" {
 
   instance_types = ["t3.micro"]
 
-  scaling_config { desired_size = 2; max_size = 3; min_size = 1 }
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
 
-  timeouts { create = "30m" }
+  timeouts {
+    create = "30m"
+  }
 }
 
 #########################################################
 # 5. Outputs
 #########################################################
-output "cluster_name"                          { value = aws_eks_cluster.eks.name }
-output "cluster_endpoint"                      { value = aws_eks_cluster.eks.endpoint }
+output "cluster_name" {
+  value = aws_eks_cluster.eks.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
 output "cluster_certificate_authority_data" {
   value     = aws_eks_cluster.eks.certificate_authority[0].data
   sensitive = true
